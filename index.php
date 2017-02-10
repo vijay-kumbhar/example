8b656e509cff54572f792ad82c153a626bdb7497
vijay-kumbhar <aboutdrim@gmail.com>
2017-02-10 14:20:14 +0530

    switch case  commit

--- index.php
+++ index.php
@@ -1,13 +1,36 @@
-
 <?php
 $d = date("D");
 
-if ($d == "Fri") {
-	echo "Have a nice !";
-} elseif ($d == "Sun") {
-	echo "Have a nice Sunday!";
-} else {
-	echo "Have a nice day!";
-}
+switch ($d) {
+case "Mon":
+	echo "Today is Monday";
+	break;
 
+case "Tue":
+	echo "Today is Tuesday";
+	break;
+
+case "Wed":
+	echo "Today is Wednesday";
+	break;
+
+case "Thu":
+	echo "Today is Thursday";
+	break;
+
+case "Fri":
+	echo "Today is Friday";
+	break;
+
+case "Sat":
+	echo "Today is Saturday";
+	break;
+
+case "Sun":
+	echo "Today is Sunday";
+	break;
+
+default:
+	echo "Wonder which day is this ?";
+}
 ?>
