7f966091a4a6fb0ecb7e20379b921d0345876db9
vijay-kumbhar <aboutdrim@gmail.com>
2017-02-10 14:29:39 +0530

    for while  commit

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
-   <?php
-$a = 0;
-$b = 0;
 
-for ($i = 0; $i < 5; $i++) {
-	$a += 10;
-	$b += 5;
-	echo ("At the end of the loop a = $a and b = $b<br>");
+<?php
+$i   = 0;
+$num = 50;
+
+while ($i < 10) {
+	$num--;
+	echo "$i++";
 
 }
-
-?>
