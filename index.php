36748a3e1191ecc2d7ba2e48f374806acf284875
vijay-kumbhar <aboutdrim@gmail.com>
2017-02-10 14:19:02 +0530

    elseIF  commit

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
-<?php
+
+      <?php
 $d = date("D");
 
 if ($d == "Fri") {
 	echo "Have a nice weekend!";
+} elseif ($d == "Sun") {
+	echo "Have a nice Sunday!";
 } else {
 	echo "Have a nice day!";
 }
